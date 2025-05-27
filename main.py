6f50458d44aea8ac20fde4210747d7f583bf180d
blossomxi <rilles.john@gmail.com>
2025-05-26 17:29:56 -0700

    added game selection

--- main.py
+++ main.py
@@ -4,24 +4,53 @@ class player:
     def __init__(self,lives):
         self.lives = lives
 class coinFlip:     
-    def headsOrTails(input):
-        coin = random.randint(1,2)
-        print("Heads or Tails")
-        input = input()
-        if coin == 1:
+    #TODO: When user enters heads or tails and when coin flip != their option player 2 gets to go first
+    def headsOrTails(option):
+        coin = random.randint(0,1)
+        print("Player 1 choose, (0)Heads or (1)Tails?")
+        option = input()
+        if coin == 0:
             print("The coinflip decides heads")
-        else:
+        elif coin == 1:
             print("The coinflip decided tails")
+        else: 
+            print('Error, choose heads or tails / 0 or 1')
+            return coinFlip.headsOrTails(option)
 
-
+class gameSelection:
     
+    def chooseOption(option):
+        #TODO: Allow user to use string aswell
+        print("How do you want to play?\n 1.) Co-op\n 2.) AI\n Please choose with 1 or 2")
+        option = int(input())
+        if option == 1:
+            coinFlip.headsOrTails(input)
+            print('Going into game now...')
+        elif option == 2:
+            coinFlip.headsOrTails(input)
+            print('Going into game now...')
+        elif option != 1 or option !=2:
+            print('Error, Please choose 1 or 2.')
+        
+        
+class GameWithCoop:
+    #Start off with two bullets
+    def game_global():
+        global bulletCount
+        bulletCount = 2
+        player1 = player(3) 
+        player2 = player(3)
+    def Game(input):
+        print("We have" + bulletCount + "in the chamber.")
+    
+        
 
     
     
 coinFlip.headsOrTails(input)
+# gameSelection.chooseOption(input)
     
-player1 = player(3) 
-player2 = player(3)
+
 robot = player(3)
 
     
